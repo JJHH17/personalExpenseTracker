27557c496748fb4e57db4be39039bcec3a31f9df
James Hatfield <james.hatfield17@icloud.com>
2025-08-03 11:29:25 +0100

    Added switch check for console iteration of app. Allows the user to fetch a list of expenses based on a given month

--- src/personalExpenseApp/UserInterface.java
+++ src/personalExpenseApp/UserInterface.java
@@ -3,9 +3,11 @@ package personalExpenseApp;
 import java.time.Month;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class UserInterface {
     private final Map<Month, ExpenseMonth> expenseMonths;
+    private final Scanner scanner = new Scanner(System.in);
 
     public UserInterface() {
         expenseMonths = new EnumMap<>(Month.class);
@@ -19,5 +21,79 @@ public class UserInterface {
         return expenseMonths;
     }
 
+    public void startApp() {
+        System.out.println("Welcome to Personal Expense Tracker");
+        boolean active = true;
 
+        while (active) {
+            System.out.println("1. View Expenses by Month \n2. Add Expense \n3. Exit");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Please enter a month (3 word abbreviation) to view expenses for that month");
+                    System.out.println("Example: JAN");
+                    System.out.println("To exit the app, enter QUIT");
+                    String monthInput = scanner.next();
+
+                    switch (monthInput.toUpperCase()) {
+                        case "QUIT":
+                            active = false;
+                            break;
+
+                        case "JAN":
+                            System.out.println(expenseMonths.get(Month.JANUARY));
+                            break;
+
+                        case "FEB":
+                            System.out.println(expenseMonths.get(Month.FEBRUARY));
+                            break;
+
+                        case "MAR":
+                            System.out.println(expenseMonths.get(Month.MARCH));
+                            break;
+
+                        case "APR":
+                            System.out.println(expenseMonths.get(Month.APRIL));
+                            break;
+
+                        case "MAY":
+                            System.out.println(expenseMonths.get(Month.MAY));
+                            break;
+
+                        case "JUN":
+                            System.out.println(expenseMonths.get(Month.JUNE));
+                            break;
+
+                        case "JUL":
+                            System.out.println(expenseMonths.get(Month.JULY));
+                            break;
+
+                        case "AUG":
+                            System.out.println(expenseMonths.get(Month.AUGUST));
+                            break;
+
+                        case "SEP":
+                            System.out.println(expenseMonths.get(Month.SEPTEMBER));
+                            break;
+
+                        case "OCT":
+                            System.out.println(expenseMonths.get(Month.OCTOBER));
+                            break;
+
+                        case "NOV":
+                            System.out.println(expenseMonths.get(Month.NOVEMBER));
+                            break;
+
+                        case "DEC":
+                            System.out.println(expenseMonths.get(Month.DECEMBER));
+                            break;
+
+                        default:
+                            System.out.println("Invalid month entered");
+                            break;
+                    }
+            }
+        }
+    }
 }

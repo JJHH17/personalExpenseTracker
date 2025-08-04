e7394297078e839a652d0eb3f9e6acce0d8b09c4
James Hatfield <james.hatfield17@icloud.com>
2025-08-04 16:10:12 +0100

    Added method that writes data to the file

--- src/personalExpenseApp/FileHandler.java
+++ src/personalExpenseApp/FileHandler.java
@@ -1,10 +1,12 @@
 package personalExpenseApp;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileHandler {
 
+    /** Method that validates whether a file exists or not, and creates it if not */
     public static void fileCreator() {
         try {
             File file = new File("personalExpenseApp.txt");
@@ -17,6 +19,22 @@ public class FileHandler {
 
         } catch (IOException e) {
             System.out.println("An error occurred when creating the file");
+            e.printStackTrace();
+        }
+    }
+
+    /** Method for writing to a given file */
+    public static void fileWriter(String fileContents) {
+        try {
+            File file = new File("personalExpenseApp.txt");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(fileContents);
+            fileWriter.close();
+            System.out.println("File written successfully");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred when writing to the file");
             e.printStackTrace();
         }
     }

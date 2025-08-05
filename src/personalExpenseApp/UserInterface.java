ae34e39a915226fba1d543795ea837586b65005a
James Hatfield <james.hatfield17@icloud.com>
2025-08-05 16:11:53 +0100

    Imported a list of month expense month objects

--- src/personalExpenseApp/UserInterface.java
+++ src/personalExpenseApp/UserInterface.java
@@ -6,152 +6,32 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class UserInterface {
-    private final Map<Month, ExpenseMonth> expenseMonths;
-    private final Scanner scanner = new Scanner(System.in);
+    private final ExpenseMonth January;
+    private final ExpenseMonth February;
+    private final ExpenseMonth March;
+    private final ExpenseMonth April;
+    private final ExpenseMonth May;
+    private final ExpenseMonth June;
+    private final ExpenseMonth July;
+    private final ExpenseMonth August;
+    private final ExpenseMonth September;
+    private final ExpenseMonth October;
+    private final ExpenseMonth November;
+    private final ExpenseMonth December;
 
     public UserInterface() {
-        expenseMonths = new EnumMap<>(Month.class);
-        // Here we're populating all months to the list based on the enums
-        for (Month month : Month.values()) {
-            expenseMonths.put(month, new ExpenseMonth(month));
-        }
-    }
+        January = new ExpenseMonth();
+        February = new ExpenseMonth();
+        March = new ExpenseMonth();
+        April = new ExpenseMonth();
+        May = new ExpenseMonth();
+        June = new ExpenseMonth();
+        July = new ExpenseMonth();
+        August = new ExpenseMonth();
+        September = new ExpenseMonth();
+        October = new ExpenseMonth();
+        November = new ExpenseMonth();
+        December = new ExpenseMonth();
 
-    public void startApp() {
-        System.out.println("Welcome to Personal Expense Tracker");
-        boolean active = true;
-
-        while (active) {
-            System.out.println("1. View Expenses by Month \n2. Add Expense \n3. Exit");
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Please enter a month (3 word abbreviation) to view expenses for that month");
-                    System.out.println("Example: JAN");
-                    System.out.println("To exit the app, enter QUIT");
-                    String monthInput = scanner.next();
-
-                    switch (monthInput.toUpperCase()) {
-                        case "QUIT":
-                            active = false;
-                            break;
-
-                        case "JAN":
-                            System.out.println(expenseMonths.get(Month.JANUARY));
-                            System.out.println(expenseMonths.get(Month.JANUARY).getMonthExpenses());
-                            break;
-
-                        case "FEB":
-                            System.out.println(expenseMonths.get(Month.FEBRUARY));
-                            System.out.println(expenseMonths.get(Month.FEBRUARY).getMonthExpenses());
-                            break;
-
-                        case "MAR":
-                            System.out.println(expenseMonths.get(Month.MARCH));
-                            break;
-
-                        case "APR":
-                            System.out.println(expenseMonths.get(Month.APRIL));
-                            break;
-
-                        case "MAY":
-                            System.out.println(expenseMonths.get(Month.MAY));
-                            break;
-
-                        case "JUN":
-                            System.out.println(expenseMonths.get(Month.JUNE));
-                            break;
-
-                        case "JUL":
-                            System.out.println(expenseMonths.get(Month.JULY));
-                            break;
-
-                        case "AUG":
-                            System.out.println(expenseMonths.get(Month.AUGUST));
-                            break;
-
-                        case "SEP":
-                            System.out.println(expenseMonths.get(Month.SEPTEMBER));
-                            break;
-
-                        case "OCT":
-                            System.out.println(expenseMonths.get(Month.OCTOBER));
-                            break;
-
-                        case "NOV":
-                            System.out.println(expenseMonths.get(Month.NOVEMBER));
-                            break;
-
-                        case "DEC":
-                            System.out.println(expenseMonths.get(Month.DECEMBER));
-                            break;
-
-                        default:
-                            System.out.println("Invalid month entered");
-                            break;
-                    }
-                    break;
-
-                case 2:
-                    scanner.nextLine();
-                    System.out.println("Please enter the expense name");
-                    String expenseName = scanner.nextLine();
-                    System.out.println("Please enter the expense amount");
-                    double expenseAmount = scanner.nextDouble();
-                    scanner.nextLine(); // Used to clear the scanner buffer
-                    System.out.println("Please enter the expense date (day/month)");
-                    String expenseDate = scanner.nextLine();
-                    System.out.println("Please enter the expense category");
-                    String expenseCategory = scanner.nextLine();
-                    System.out.println("Please enter the expense notes");
-                    String expenseNotes = scanner.nextLine();
-
-                    Expenses newExpense = new Expenses(expenseName, expenseAmount, expenseDate, expenseCategory, expenseNotes);
-
-                    Month expenseMonth = null;
-                    if (expenseDate.toLowerCase().contains("jan")) {
-                        expenseMonth = Month.JANUARY;
-                    } else if (expenseDate.toLowerCase().contains("feb")) {
-                        expenseMonth = Month.FEBRUARY;
-                    } else if (expenseDate.toLowerCase().contains("mar")) {
-                        expenseMonth = Month.MARCH;
-                    } else if (expenseDate.toLowerCase().contains("apr")) {
-                        expenseMonth = Month.APRIL;
-                    } else if (expenseDate.toLowerCase().contains("may")) {
-                        expenseMonth = Month.MAY;
-                    } else if (expenseDate.toLowerCase().contains("jun")) {
-                        expenseMonth = Month.JUNE;
-                    } else if (expenseDate.toLowerCase().contains("jul")) {
-                        expenseMonth = Month.JULY;
-                    } else if (expenseDate.toLowerCase().contains("aug")) {
-                        expenseMonth = Month.AUGUST;
-                    } else if (expenseDate.toLowerCase().contains("sep")) {
-                        expenseMonth = Month.SEPTEMBER;
-                    } else if (expenseDate.toLowerCase().contains("oct")) {
-                        expenseMonth = Month.OCTOBER;
-                    } else if (expenseDate.toLowerCase().contains("nov")) {
-                        expenseMonth = Month.NOVEMBER;
-                    } else if (expenseDate.toLowerCase().contains("dec")) {
-                        expenseMonth = Month.DECEMBER;
-                    }
-
-                    if (expenseMonth != null) {
-                        ExpenseMonth monthObject = expenseMonths.get(expenseMonth);
-                        ExpenseDay expenseDay = new ExpenseDay(expenseDate.split("/")[0], expenseDate.split("/")[1]);
-                        expenseDay.addExpense(newExpense);
-                        monthObject.addExpenseDay(expenseDay);
-                        expenseMonths.put(expenseMonth, monthObject);
-                        System.out.println("Expense added successfully");
-                    } else {
-                        System.out.println("Invalid date entered");
-                    }
-                    break;
-
-                case 3:
-                    active = false;
-                    break;
-            }
-        }
     }
 }

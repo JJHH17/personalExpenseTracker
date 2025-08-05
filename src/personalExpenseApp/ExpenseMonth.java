857458b995b968d520fb1658b25f5aa91327fad1
James Hatfield <james.hatfield17@icloud.com>
2025-08-05 16:06:00 +0100

    Refactored ExpenseMonth class, which takes in a list of expenses based on their month

--- src/personalExpenseApp/ExpenseMonth.java
+++ src/personalExpenseApp/ExpenseMonth.java
@@ -1,45 +1,22 @@
 package personalExpenseApp;
 
-import java.time.Month;
 import java.util.ArrayList;
 
 public class ExpenseMonth {
-    private final Month month;
-    private final ArrayList<ExpenseDay> expenseDays;
+    private ArrayList<Expenses> expenses;
 
-    public ExpenseMonth(Month month) {
-        this.month = month;
-        this.expenseDays = new ArrayList<>();
-    }
-
-    public Month getMonth() {
-        return month;
-    }
-
-    public ArrayList<ExpenseDay> getExpenseDays() {
-        return expenseDays;
-    }
-
-    public void addExpenseDay(ExpenseDay expenseDay) {
-        expenseDays.add(expenseDay);
+    public ExpenseMonth() {
+        this.expenses = new ArrayList<>();
     }
 
     @Override
     public String toString() {
-        return "ExpenseMonth{" +
-                "Month=" + month +
-                ", number of expense days=" + expenseDays.size() +
-                "}";
-    }
+        String response = "";
 
-    /** Used to provide more in-depth return of expenses */
-    public String getMonthExpenses() {
-        ArrayList<ExpenseDay> monthExpenses = new ArrayList<>();
-        for (ExpenseDay expenseDay : expenseDays) {
-            if (expenseDay.getExpenseTotal() > 0) {
-                monthExpenses.add(expenseDay);
-            }
+        for (Expenses expense : expenses) {
+            response += expense ;
         }
-        return monthExpenses.toString();
+
+        return response;
     }
 }

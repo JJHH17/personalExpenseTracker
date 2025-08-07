86f2269c3cb40fbaa7229e2ab9987c61c797cfa8
James Hatfield <james.hatfield17@icloud.com>
2025-08-07 19:04:18 +0100

    Refactored return info method which is used to return data from an existing file

--- src/personalExpenseApp/ExpenseMonth.java
+++ src/personalExpenseApp/ExpenseMonth.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class ExpenseMonth {
     private final ArrayList<Expenses> expenses;
+    private final FileHandler file;
 
     public ExpenseMonth() {
         this.expenses = new ArrayList<>();
+        this.file = new FileHandler();
     }
 
     public void addEntry(Expenses expense) {
@@ -14,11 +16,13 @@ public class ExpenseMonth {
         FileHandler.fileWriter(expense.toString(), expense.getExpenseMonth());
     }
 
-    @Override
-    public String toString() {
+    public String returnInfo(String month) {
+        ArrayList<Expenses> fileData = file.fileReader(month);
+
         StringBuilder response = new StringBuilder();
 
-        for (Expenses expense : expenses) {
+        // Pushing the entries from the existing file to the array
+        for (Expenses expense : fileData) {
             response.append("Expense Name: ").append(expense.getExpenseName()).append(",");
             response.append("Amount: ").append(expense.getExpenseAmount()).append(",");
             response.append("Category: ").append(expense.getExpenseCategory()).append(",");

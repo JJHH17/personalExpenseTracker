8f897465de4e24328af2595e7a255e347cb6911a
James Hatfield <james.hatfield17@icloud.com>
2025-08-09 16:10:02 +0100

    Created simple methods for JavaFX frontend creation. A frontend page now appears when project runs

--- src/personalExpenseApp/Main.java
+++ src/personalExpenseApp/Main.java
@@ -1,8 +1,34 @@
 package personalExpenseApp;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
-        userInterface.start();
+//        UserInterface userInterface = new UserInterface();
+//        userInterface.start();
+        launcher();
+    }
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Personal Expense Tracker");
+        Label title = new Label("Personal Expense Tracker");
+        VBox layout = new VBox(10);
+        Label label = new Label("Hello World!");
+        layout.getChildren().addAll(title, label);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void launcher() {
+        launch();
     }
 }
